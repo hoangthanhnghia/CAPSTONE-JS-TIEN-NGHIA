c973eea1e18646310589386a53ef090dfc65a7d6
Trinh Minh Tien <tien61266@gmail.com>
2022-12-19 13:04:33 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -1,76 +1,160 @@
 var productList = [];
 var card = [];
 
+function fetchProductList() {
+  //   console.log("send request");
 
-function fetchProductList(){
-    console.log("send request");
-
-    var promise = axios({
-        url: "https://639bfea842e3ad69272413bc.mockapi.io/products",
-        method: "GET",
+  var promise = axios({
+    url: "https://639bfea842e3ad69272413bc.mockapi.io/products",
+    method: "GET",
+  });
+  promise
+    .then(function (res) {
+      productList = res.data;
+      console.log("ok", productList);
+      renderDevice();
+    })
+    .catch(function (err) {
+      console.log("error", err);
     });
-
-    function handleSuccess(res) {
-        console.log("success", res);
-    }
-
-    function handleError(err) {
-        console.log("error", err);
-    }
-
-    promise.then(handleSuccess);
-    promise.catch(handleError);
 }
 
 window.onload = function () {
-    fetchProductList();
+  fetchProductList();
 };
 
-function genderChanged(obj)
-{
-    var message = document.getElementById('show_message');
-    var value = obj.value;
-    if (value === ''){
-        message.innerHTML = "Bạn chưa chọn loại sản phẩm";
-    }
-    else if (value === 'iphone'){
-        message.innerHTML = "Bạn đã chọn Iphone";
-        for(var i = 0; i < productList.length; i++){
-            if(productList.type[i] === 'iphone'){
-                // duyệt vòng for chọn type === iphone cộng dồn vào mảng
+function genderChanged(obj) {
+  var message = document.getElementById("show_message");
+  var value = obj.value;
+  if (value === "") {
+    message.innerHTML = "Bạn chưa chọn loại sản phẩm";
+  } else if (value === "iphone") {
+    message.innerHTML = "Bạn đã chọn Iphone";
+    for (var i = 0; i < productList.length; i++) {
+      if (productList.type[i] === "iphone") {
+        // duyệt vòng for chọn type === iphone cộng dồn vào mảng
 
-
-                // khi type === 'iphone' thì in ra danh sách iphone
-                renderProductsIphone();
-            }
-        }
+        // khi type === 'iphone' thì in ra danh sách iphone
+        renderProductsIphone();
+      }
     }
-    else if (value === 'samsung'){
-        message.innerHTML = "Bạn đã chọn Samsung";
-        // duyệt vòng for chọn type === samsung cộng dồn vào mảng
-                
+  } else if (value === "samsung") {
+    message.innerHTML = "Bạn đã chọn Samsung";
+    // duyệt vòng for chọn type === samsung cộng dồn vào mảng
 
-        // khi type === 'samsung' thì in ra danh sách samsung
-        renderProductsSamsung();
-    }
+    // khi type === 'samsung' thì in ra danh sách samsung
+    renderProductsSamsung();
+  }
 }
 
 // cần 2 danh sách của iphone và samsung để in ra màn hình khi type === '1 trong 2'
-// 
+//
 function renderProductsIphone() {
-    var htmlIphone = '';
-    for(var i = 0; i < productList.length; i++){
-        htmlIphone += `<div>${productList[i].type}</div>
-        `
-    }
+  var htmlIphone = "";
+  for (var i = 0; i < productList.length; i++) {
+    htmlIphone += `<div>${productList[i].type}</div>
+        `;
+  }
 }
 
 function renderProductsSamsung() {
-    var htmlSamsung = '';
-    for(var i = 0; i < productList.length; i++){
+  var htmlSamsung = "";
+  for (var i = 0; i < productList.length; i++) {}
+}
 
-    }
+function renderDevice() {
+  // data = data || productList;
+  // console.log(productList)
+  var html = "";
+  for (var i = 0; i < productList.length; i++) {
+    html += `<div class="col-3 p-0">
+    <div class="info">
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <img
+            src= ${productList[i].img}
+            class="card-img-top"
+            alt="Iphone14ProMaxBlack"
+          />
+          <h5 class="card-title mt-2">${productList[i].name}</h5>
+          <p class="card-text">
+          ${productList[i].desc}
+          </p>
+          <button onclick="shopMyCart(this)" class="btn btn-primary">Add To Cart</button>
+        </div> 
+      </div>
+    </div>
+  </div>`;
+  }
+  document.getElementById("table").innerHTML = html;
+}
+
+// var btn = document.querySelectorAll(".btn");
+// console.log(btn);
+// btn.forEach(function (button, index) {
+//   button.addEventListener("click", function (event) {
+//     {
+//       var btnItem = event.target;
+//       var product = btnItem.parentElement;
+//       var productImg = product.querySelector("img").src;
+//       var productName = product.querySelector("h5").innerText;
+//       console.log(productName);
+//     }
+//   });
+// });
+function shopMyCart() {
+  var myCart = "";
+  for (var i = 0; i < productList.length; i++) {
+    myCart += `<tbody>
+    <div class="row mx-0 align-items-center">
+      <div class="col">
+        <img
+          style="max-width: 100%"
+          src=${productList[i].img}
+          alt=""
+        />
+      </div>
+      <div class="col">
+        <p>${productList[i].name}</p>
+      </div>
+      <div class="col">
+        <p>${productList[i].price}</p>
+      </div>
+      <div class="col main__nav">
+        <button style="border-radius: 50%" class="btn-qty">
+          <i class="fa-solid fa-chevron-left"></i>
+        </button>
+        <span class="cantidad-elementos">0</span>
+        <button style="border-radius: 50%" class="btn-qty">
+          <i class="fa-solid fa-angle-right"></i>
+        </button>
+      </div>
+      <div class="col">
+        <p class="precio-final">12.000.000</p>
+      </div>
+      <div class="col" style="text-align: center">
+        <button id="btnDelete" class="btn btn-danger">
+          <i class="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
+    </div>
+    <hr style="width: 96%" />
+  </tbody>`;
+  }
+  document.getElementById("bodyMyCart").innerHTML = myCart;
 }
 
 
-
+var btn = document.querySelectorAll(".btn.btn-primary");
+ console.log(btn)
+btn.forEach(function (button, index) {
+  button.addEventListener("click", function (event) {
+    {
+      var btnItem = event.target;
+      var product = btnItem.parentElement;
+   var productImg = product.querySelector("img").src
+   var productName = product.querySelector("h5").innerText;
+      console.log(productImg);
+    }
+  });
+});
