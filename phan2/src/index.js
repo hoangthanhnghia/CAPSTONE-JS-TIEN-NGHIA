d94c6b2628a9342c56049f99e8318d0a9381b3dd
hoang thanh nghia <thanhnghia1712@gmail.com>
2022-12-21 12:21:13 +0700

    push index.js p2 + admin.js p2

---
+++ phan2/src/index.js
@@ -0,0 +1,28 @@
+var productList = [];
+
+function createProduct() {
+    var name = document.getElementById("");
+    var price = document.getElementById("");
+    var img = document.getElementById("");
+    var desc = document.getElementById("");
+
+    var admin = new Admin(
+        name,
+        price,
+        img,
+        desc
+    );
+
+    var promise = axios({
+        url: "https://639bfea842e3ad69272413bc.mockapi.io/phan2",
+        method: "POST",
+        // request body => POST, PUT, PATCH
+        data: admin,
+    }).then(function (res){
+        alert("Thêm thành công!");
+        fetchProductList();
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+}
---
+++ phan2/src/model/admin.js
@@ -0,0 +1,6 @@
+function Admin(name, price, img, desc){
+    this.name = name;
+    this.price = price;
+    this.img = img;
+    this.desc = desc;
+}
